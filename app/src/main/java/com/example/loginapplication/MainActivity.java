c0ae8a97dd27ff7c426fa5bc37d00c10b09fcc2e
sajibsaha4647 <sksaha4648@gmail.com>
2022-01-01 01:42:46 +0600

    complete login application

--- app/src/main/java/com/example/loginapplication/MainActivity.java
+++ app/src/main/java/com/example/loginapplication/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.loginapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextone,editTexttwo;
+    private Button button;
+    private TextView textView;
+    private int counter = 3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,31 @@ public class MainActivity extends AppCompatActivity {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(this.getResources().getColor(R.color.black));
         }
+
+        editTextone = findViewById(R.id.emailid);
+        editTexttwo = findViewById(R.id.passwordid);
+        button = findViewById(R.id.loginbtn);
+        textView = findViewById(R.id.attemptthree);
+        textView.setText("number of attempt : 3");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextone.getText().toString();
+                String password = editTexttwo.getText().toString();
+                if(email.equals("admin") && password.equals("1234")){
+                    Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                    startActivity(intent);
+                }else{
+                    counter--;
+                    textView.setText("number of attempt :" + counter);
+                    if(counter == 0){
+                        button.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+
     }
 }
